8772eff1b8cff3da36fc6085d2ef5d81f7d980b3
Botabay <tohgl49@gmail.com>
2023-03-28 13:35:50 +0600

    described the MessagePropsType type in the Message.tsx

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,8 +14,19 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
-
+export type MessageType = {
+    id:number,
+    user: userType,
+    message: messageType,
+}
+type userType = {
+    avatar:typeof avatar,// i'm not sure for the record!!!!!!!!
+    name: string,
+}
+type messageType = {
+    text:string,
+    time: string,
+}
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
