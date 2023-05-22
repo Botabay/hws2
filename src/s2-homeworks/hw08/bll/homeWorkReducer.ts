7835909e219a5b03468cce5486253738311de031
Botabay <tohgl49@gmail.com>
2023-05-22 21:32:15 +0600

    homework8 is done without css

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,19 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const upSort = (a: UserType, b: UserType) => a.name < b.name ? -1 : 1;
+            const downSort = (a: UserType, b: UserType) => a.name > b.name ? -1 : 1;
+            if (action.payload === 'up') return [...state.sort(upSort)];
+            else return [...state.sort(downSort)]// need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload) // need to fix
         }
         default:
             return state
