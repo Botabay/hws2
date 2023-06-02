fa24c474d600c11b5eebc5c90e53fac80251e796
Botabay <tohgl49@gmail.com>
2023-06-02 23:58:55 +0600

    lesson is done, iteration1

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -43,9 +43,10 @@ function Clock() {
     const stringDate = dateFormatter.format(new Date()) || 'date->date' || <br /> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const weekdayFormatter = new Intl.DateTimeFormat("en-US", { weekday: 'long', month: 'long' });
+    const weekdayFormatter = new Intl.DateTimeFormat("en-US", { weekday: 'long' });
     const stringDay = weekdayFormatter.format(new Date()) || 'date->day' || <br /> // пишут студенты
-    const stringMonth = 'date->month' || <br /> // пишут студенты
+    const monthFormatter = new Intl.DateTimeFormat("en-US", { month: 'long' });
+    const stringMonth = monthFormatter.format(new Date()) || 'date->month' || <br /> // пишут студенты
 
     return (
         <div className={s.clock}>
