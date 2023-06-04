87dccb5d52a2cd33a0401921ef83ea9f07bd8e1d
Botabay <tohgl49@gmail.com>
2023-06-04 17:32:22 +0600

    lesson10 is done, totally

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,10 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <svg width="50" height="50">
+        <circle r="20" cx="25" cy="25" className={s.progress}></circle>
+    </svg>
+</div>
+
+//for variants 1,2
+// export const Loader = () => <div className={s.loader}/>
